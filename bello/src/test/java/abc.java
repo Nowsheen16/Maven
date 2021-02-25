617253c5205843d1dc64107aa58c80580a7fb4cf
Nowsheen16 <nowsheen.mufti@tothenew.com>
2021-02-25 15:48:52 +0530

    Initialised Commit

---
+++ bello/src/test/java/abc.java
@@ -0,0 +1,13 @@
+import org.junit.Assert.*;
+
+import static org.junit.Assert.fail;
+
+
+public class abc {
+    @org.junit.Test
+    public void test(){
+//        fail();
+
+    }
+
+}
---
+++ cello/src/test/java/harshu.java
@@ -0,0 +1,13 @@
+import org.junit.Assert.*;
+
+import static org.junit.Assert.fail;
+
+
+public class harshu {
+    @org.junit.Test
+    public void test(){
+//        fail();
+
+    }
+
+}
